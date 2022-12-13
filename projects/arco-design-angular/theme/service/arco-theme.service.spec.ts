2c7ec1ac5cf39ce87858c5e8f3ebf9762a773884
fan <xiaofanms@outlook.com>
2022-12-13 23:10:52 +0800

    解决服务类单元测试报错的问题。

--- projects/arco-design-angular/theme/service/arco-theme.service.spec.ts
+++ projects/arco-design-angular/theme/service/arco-theme.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ArcoThemeModule } from 'arco-design-angular/theme';
 
 import { ArcoThemeService } from './arco-theme.service';
 
@@ -6,7 +7,9 @@ describe('ArcoThemeService', () => {
   let service: ArcoThemeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ArcoThemeModule]
+    });
     service = TestBed.inject(ArcoThemeService);
   });
 
