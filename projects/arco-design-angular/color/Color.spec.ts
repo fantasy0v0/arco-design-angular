a903eecf3ee45dc92c5e0b3acf613868ccadacf4
fan <xiaofanms@outlook.com>
2022-12-15 23:30:28 +0800

    细节调整。

--- projects/arco-design-angular/color/Color.spec.ts
+++ projects/arco-design-angular/color/Color.spec.ts
@@ -17,19 +17,48 @@ describe('Arco Color', () => {
   });
 
   it('print css var', () => {
-    const _Colors = Colors as { [key: string]: Color };
-    let buff = '';
-    for (let key of Object.keys(_Colors)) {
-      const color = _Colors[key];
-      const gradients = color.gradients();
-      let _buff = '';
-      for (const index in gradients) {
-        const gradient = gradients[index];
-        _buff += `--${color.name()}-${+index + 1}: ${gradient}\n`;
+    let genColor = (dark: boolean = false) => {
+      const _Colors = Colors as { [key: string]: Color };
+      let buff = '$';
+      if (dark) {
+        buff += 'dart-colors';
+      } else {
+        buff += 'colors';
       }
-      buff += _buff + '\r\n';
+      buff += ': (';
+      let first = true;
+      for (let key of Object.keys(_Colors)) {
+        const color = _Colors[key];
+        console.log(color.gradients(true, 'rgb'));
+        const gradients = dark ? color.gradients(true) : color.gradients();
+        let _buff = '';
+        if (first) {
+          first = false;
+        } else {
+          _buff += ', ';
+        }
+        _buff += `"${color.name()}":(`;
+        let _first = true;
+        for (const index in gradients) {
+          if (_first) {
+            _first = false;
+          } else {
+            _buff += ', ';
+          }
+          const gradient = gradients[index];
+          _buff += gradient;
+        }
+        buff += _buff;
+        buff += ')';
+      }
+      buff += ');';
+      return buff;
+
     }
-    console.log(buff);
+    let colors = genColor();
+    console.log(colors);
+    colors = genColor(true);
+    console.log(colors);
     expect(true).toBe(true);
   });
 });
