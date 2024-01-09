a40ac5af4d97a07cbabf8e37eb7a2aa138f90a10
fan <xiaofanms@outlook.com>
2024-01-09 18:42:50 +0800

    提交修改。

--- projects/arco-design-angular/button/button.component.ts
+++ projects/arco-design-angular/button/button.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'button[arco-button]',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent {
 
