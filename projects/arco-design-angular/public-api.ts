50a18a7c455eb7f0395dfa82100707a4a1a268e0
fan <xiaofanms@outlook.com>
2024-06-13 14:48:56 +0800

    升级到18。

--- projects/arco-design-angular/public-api.ts
+++ projects/arco-design-angular/public-api.ts
@@ -1,2 +1,6 @@
-// https://github.com/ng-packagr/ng-packagr/issues/1655
-export default void 0;
+export * from './core';
+export * from './button';
+export * from './color';
+export * from './grid';
+export * from './icon';
+export * from './theme';
--- projects/arco-design-angular/theme/ArcoTheme.ts
+++ projects/arco-design-angular/theme/ArcoTheme.ts
@@ -1,4 +1,4 @@
-import { Color } from "arco-design-angular/color";
+import { Color } from "../color";
 import { ThemeMode } from "./ThemeMode";
 
 /**
